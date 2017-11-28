0732b1ec51d5c3cb4fc2275e1d42b118f48af5f2
anjacl <anja.clundgren@gmail.com>
2017-11-28 19:57:51 +0100

    Added class teacher.

---
+++ teacher.js
@@ -0,0 +1,25 @@
+/*jshint esversion:6*/
+
+
+let myTeacher = new Teacher ('anna', 'Karlsson', '012345');
+myTeacher.startTeachingCourse('engelska');
+myTeacher.startTeachingCourse('Tyska');
+console.log(myTeacher);
+
+let myTeacher2 = new Teacher ('ann', 'Karlsson', '012345');
+myTeacher2.startTeachingCourse('Svenska');
+console.log(myTeacher2);
+
+
+let myTeacher = new Teacher ('anna', 'Karlsson', '012345');
+myTeacher.startTeachingCourse('engelska');
+myTeacher.startTeachingCourse('Tyska');
+console.log(myTeacher);
+
+let myTeacher2 = new Teacher ('ann', 'Karlsson', '012345');
+myTeacher2.startTeachingCourse('Svenska');
+console.log(myTeacher2);
+
+
+let myStudent = new Student ('A', 'Karlsson', '012345', '3');
+console.log(myStudent);
